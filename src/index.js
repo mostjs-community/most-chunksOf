89ac93a08401d0215a27b0b25f099ecc3d659128
Brian Cavalier <brian@briancavalier.com>
2017-01-09 13:14:57 -0500

    Preallocate chunk buffer. Add minimal tests (#6)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /** @license MIT License (c) copyright 2016 original author or authors */
 
-import { FullBufferSink } from './BufferSinks'
+import { ChunksOfSink } from './ChunksOfSink'
 import { curry2 } from '@most/prelude'
 
-const chunksStream = (n, stream, constr) => new stream.constructor({
-    run: (sink, scheduler) => stream.source.run(new constr(n, sink), scheduler)
+const chunksOfStream = (n, stream) => new stream.constructor({
+  run: (sink, scheduler) => stream.source.run(new ChunksOfSink(n, sink), scheduler)
 })
 
-export const chunksOf = curry2((n, stream) => chunksStream(n, stream, FullBufferSink))
+export const chunksOf = curry2(chunksOfStream)
--- test/index-test.js
+++ test/index-test.js
@@ -3,12 +3,26 @@
 import {describe, it} from 'mocha'
 import assert from 'assert'
 
-import awesome from '../src/index'
-
-// TODO: Write more tests
+import { from, reduce } from 'most'
+import { chunksOf } from '../src/index'
 
 describe('most-chunksOf', () => {
-  it('should be awesome', () => {
-    assert(awesome(true))
+  it('should create chunks of size n', () => {
+    const chunkSize = 10
+    const a = Array.from(Array(chunkSize * 10)).map((_, i) => i)
+
+    const s = chunksOf(chunkSize, from(a))
+    return reduce((b, chunk) => b.concat(chunk), [], s)
+      .then(b => assert.deepEqual(a, b))
+  })
+
+  it('should include trailing events', () => {
+    const chunkSize = 10
+    const extra = chunkSize - 1
+    const a = Array.from(Array((chunkSize * 10) + extra)).map((_, i) => i)
+
+    const s = chunksOf(chunkSize, from(a))
+    return reduce((b, chunk) => b.concat(chunk), [], s)
+      .then(b => assert.deepEqual(a, b))
   })
 })
