ee1c5d243cfc9a7a8af22b95731fcdccb4212981
David Chase <dannopolsky@freepeople.com>
2017-01-08 23:27:59 -0500

    simplify API :fire:

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 /** @license MIT License (c) copyright 2016 original author or authors */
 
-import { FullBufferSink, PartialBufferSink } from './BufferSinks'
+import { FullBufferSink } from './BufferSinks'
 
 const chunksStream = (n, stream, constr) => new stream.constructor({
     run: (sink, scheduler) => stream.source.run(new constr(n, sink), scheduler)
 })
 
 export const chunksOf = (n, stream) => chunksStream(n, stream, FullBufferSink)
-export const chunkEvery = (n, stream) => chunksStream(n, stream, PartialBufferSink)
